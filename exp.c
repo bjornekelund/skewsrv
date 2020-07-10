efb7d4ae985330d74477ff554ec00c95561f04fe
Bjorn Ekelund <bjorn@ekelund.nu>
2020-07-10 23:56:20 +0200

    Updates

--- exp.c
+++ exp.c
@@ -11,14 +11,16 @@ int main (void)
     void *context = zmq_ctx_new ();
     void *requester = zmq_socket (context, ZMQ_SUB);
     int status;
-    zmq_connect (requester, "tcp://138.201.156.239:5565");
-    (void)zmq_setsockopt(requester, ZMQ_SUBSCRIBE, "",0);
+    zmq_connect (requester, "tcp://138.201.156.239:5566");
+    (void)zmq_setsockopt(requester, ZMQ_SUBSCRIBE, "", 0);
     int request_nbr;
-    char buffer [BUFLEN];
+    char topic[BUFLEN], buffer [BUFLEN];
 
-    for (request_nbr = 0; request_nbr != 7; request_nbr++) {
+    for (request_nbr = 0; request_nbr < 100; request_nbr++) {
+        status = zmq_recv (requester, topic, BUFLEN, 0);
+	printf("STAT=%d: topic=%s\n\n", status, buffer);
         status = zmq_recv (requester, buffer, BUFLEN, 0);
-	printf("STAT=%d, MSG=\"%s\"\n\n", status, buffer);
+	printf("STAT=%d: buffer=%s\n\n", status, buffer);
     }
     zmq_close (requester);
     zmq_ctx_destroy (context);
