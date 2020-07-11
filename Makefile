7b4abee050093b8e4806d38859d8712b788bf949
Bjorn Ekelund <bjorn@ekelund.nu>
2020-07-11 13:54:46 +0200

    Updates

--- Makefile
+++ Makefile
@@ -2,23 +2,19 @@ cc		= gcc
 gcc		= ${cc} -Wall
 lint		= cppcheck
 
-all: 		skew exp
+all: 		skew exp exp2
 
 skew:		skew.o Makefile
-		$(gcc) -o skew skew.o -lm
+			$(gcc) -o skew skew.o -lm
 
 skew.o:		skew.c Makefile
-		$(gcc) -c skew.c
+			$(gcc) -c skew.c
 
-exp:		exp.o Makefile
-		$(gcc) -o exp exp.o -lm -lzmq
-
-exp.o:		exp.c Makefile
-		$(gcc) -c exp.c
-
-hwserver3:	hwserver3.c Makefile
-		gcc -c hwserver3.c -o hwserver3 -lzmq
-
+exp:		exp.c Makefile
+			$(gcc) -o exp exp.c -lm -lzmq
+			
+exp2:		exp2.c Makefile
+			${gcc} exp2.c -o exp2 -lm -lzmq
 clean:
 		rm -f *.o
 
--- exp.c
+++ exp.c
@@ -1,28 +1,60 @@
-//  Hello World client
-#include <zmq.h>
-#include <string.h>
+#include "zmq.h"
 #include <stdio.h>
-#include <unistd.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
 #define BUFLEN 8192
+#define STRLEN 16
+#define TSLEN 20
+#define FMT "%Y-%m-%d %H:%M:%S"
+
 int main (void)
 {
     printf ("Connecting to server...\n");
     void *context = zmq_ctx_new ();
     void *requester = zmq_socket (context, ZMQ_SUB);
-    int status;
     zmq_connect (requester, "tcp://138.201.156.239:5566");
     (void)zmq_setsockopt(requester, ZMQ_SUBSCRIBE, "", 0);
-    int request_nbr;
-    char topic[BUFLEN], buffer [BUFLEN];
+    char buffer[BUFLEN], string[BUFLEN];
+    int size;
+    char de[STRLEN], dx[STRLEN], timestring1[TSLEN], 
+        timestring2[TSLEN], extradata[STRLEN];
+    int x1, x2, x3, x4, snr, speed, got, mode;
+    time_t time1, time2;
+    float freq;
+    struct tm *stime;
+    
+    while (true)
+    {
+        size = zmq_recv (requester, buffer, BUFLEN, 0);
+        memcpy(string, buffer, size);
+        string[size] = 0;       
+        
+        if (strncmp(buffer, "PROD_SPOT_", 10) == 0) 
+        {
+            size = zmq_recv (requester, buffer, BUFLEN, 0);
+            memcpy(string, buffer, size);
+            string[size] = 0;
+            //  QRG     call    spotter               spotted         received
+            // 7029.00|DL2DXA/W|9A1CIG|1|1|10|20|1|1|1594465469081|1594465469280|rx1
 
-    for (request_nbr = 0; request_nbr < 100; request_nbr++) {
-        status = zmq_recv (requester, topic, BUFLEN, 0);
-	printf("STAT=%d: topic=%s\n\n", status, buffer);
-        status = zmq_recv (requester, buffer, BUFLEN, 0);
-	printf("STAT=%d: buffer=%s\n\n", status, buffer);
+        got = sscanf(string, "%f|%[^|]|%[^|]|%d|%d|%d|%d|%d|%d|%ld|%ld|%s",
+            &freq, dx, de, &x1, &x2, &snr, &speed, &x3, &x4, &time1, &time2, extradata);
+
+        stime = gmtime(&time1);
+        (void)strftime(timestring1, TSLEN, FMT, stime);
+
+        stime = gmtime(&time2);
+        (void)strftime(timestring2, TSLEN, FMT, stime);
+
+        if (got == 12)
+            printf("Freq=%7.1f DX=%9s DE=%9s X1=%2d x2=%2d SNR=%2d SP=%2d X3=%2d X4=%2d t1=%s t2=%s EX=%s\n", 
+                freq, dx, de, x1, x2, snr, speed, x3, x3, timestring1, timestring2, extradata);
+        }
     }
     zmq_close (requester);
     zmq_ctx_destroy (context);
+
     return 0;
 }
