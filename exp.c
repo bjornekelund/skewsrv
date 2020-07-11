42ab5167c15c7121b2d91fbe72e1711374cda2cd
Bjorn Ekelund <bjorn@ekelund.nu>
2020-07-11 21:29:20 +0200

    Updates

--- exp.c
+++ exp.c
@@ -18,18 +18,21 @@ int main (void)
     (void)zmq_setsockopt(requester, ZMQ_SUBSCRIBE, "", 0);
     char buffer[BUFLEN], string[BUFLEN];
     int size;
-    char de[STRLEN], dx[STRLEN], timestring1[TSLEN], 
-        timestring2[TSLEN], extradata[STRLEN];
-    int x1, x2, x3, x4, snr, speed, got, mode;
-    time_t time1, time2;
+    char de[STRLEN], dx[STRLEN], timestring[TSLEN], 
+        extradata[STRLEN];
+    int snr, speed, got;
+    time_t jstime1, jstime2, spottime;
     float freq;
     struct tm *stime;
+    // char field[12][STRLEN];
+    int spot_type, mode, ntp;
+    float base_freq;
     
     while (true)
     {
         size = zmq_recv (requester, buffer, BUFLEN, 0);
         memcpy(string, buffer, size);
-        string[size] = 0;       
+        string[size] = 0;
         
         if (strncmp(buffer, "PROD_SPOT_", 10) == 0) 
         {
@@ -39,18 +42,17 @@ int main (void)
             //  QRG     call    spotter               spotted         received
             // 7029.00|DL2DXA/W|9A1CIG|1|1|10|20|1|1|1594465469081|1594465469280|rx1
 
-        got = sscanf(string, "%f|%[^|]|%[^|]|%d|%d|%d|%d|%d|%d|%ld|%ld|%s",
-            &freq, dx, de, &x1, &x2, &snr, &speed, &x3, &x4, &time1, &time2, extradata);
+            got = sscanf(string, "%f|%[^|]|%[^|]|%d|%f|%d|%d|%d|%d|%ld|%ld|%s",
+                &freq, dx, de, &spot_type, &base_freq, &snr, &speed, &mode, &ntp, &jstime1, &jstime2, extradata);
 
-        stime = gmtime(&time1);
-        (void)strftime(timestring1, TSLEN, FMT, stime);
+            spottime = jstime2 / 1000;
+            
+            stime = gmtime(&spottime);
+            (void)strftime(timestring, TSLEN, FMT, stime);
 
-        stime = gmtime(&time2);
-        (void)strftime(timestring2, TSLEN, FMT, stime);
-
-        if (got == 12)
-            printf("Freq=%7.1f DX=%9s DE=%9s X1=%2d x2=%2d SNR=%2d SP=%2d X3=%2d X4=%2d t1=%s t2=%s EX=%s\n", 
-                freq, dx, de, x1, x2, snr, speed, x3, x3, timestring1, timestring2, extradata);
+            // printf("%s\n", string);
+            printf("got=%2d Freq=%7.1f DX=%9s DE=%9s type=%2d bf=%2.1f SNR=%2d SP=%2d md=%2d ntp=%1d t=%s EX=%s\n", 
+                got, freq, dx, de, spot_type, base_freq, snr, speed, mode, ntp, timestring, extradata);
         }
     }
     zmq_close (requester);
