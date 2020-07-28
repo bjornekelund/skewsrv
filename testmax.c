7fe07dc7078d8ed3f84510ca7b1fb1b084677e0b
Bjorn Ekelund <bjorn@ekelund.nu>
2020-07-28 09:53:24 +0200

    Updates

--- testmax.c
+++ testmax.c
@@ -11,7 +11,8 @@ int main(int argc, char *argv[])
     unsigned long int unsignedlongint = ULONG_MAX;
     long int longint = LONG_MAX;
     
-    printf("max unsigned long int = %lu\nmax long int = %ld\n", unsignedlongint, longint);
+    printf("max unsigned long int = %lu = %.2e\n", unsignedlongint, (double)unsignedlongint);
+    printf("max long int = %ld = %.2e\n", longint, (double)longint);
 
     return 0;
 }
